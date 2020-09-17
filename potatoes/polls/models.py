27bbf3155d76041182da459072e8142f82ae0c1d
Rose Li <li.rose11719@gmail.com>
2020-09-17 16:01:32 -0400

    Add question and choice objects to polls endpoint

--- potatoes/polls/models.py
+++ potatoes/polls/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+# Django built-in ORM
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self): # define string representation of the object
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # delete this with its associated question
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self): # define string representation of the object
+        return self.choice_text
